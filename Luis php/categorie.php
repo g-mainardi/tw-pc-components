65cb90342e22f32366a6fbf4c5ab8989b6f98448
LuKe2_Ink <56109204+LuKe2Ink@users.noreply.github.com>
2022-01-05 12:08:29 +0100

    bottoni venditori su categorie prodotto

--- Luis php/categorie.php
+++ Luis php/categorie.php
@@ -4,6 +4,7 @@ require_once 'required.php';
 $SetParameters["titolo"] = "Categorie";
 $SetParameters["file"] = "categoria_pezzi.php";
 $SetParameters["categoria"] = $_GET["categoria"];
+$SetParameters["venditori"] = $dbh->getVenditori($_GET["categoria"]);
 
 if(isset($_GET["venditore"])){
     $SetParameters["prodotti"] = $dbh->getVenditoreProdotti($_GET["categoria"], $_GET["venditore"]);
