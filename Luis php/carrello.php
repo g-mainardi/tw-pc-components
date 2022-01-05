cf90dc97c827d221501674d309c1ded313b80e39
g-mainardi <gmainardi11@gmail.com>
2022-01-05 17:42:15 +0100

    Sistemato parametrizzazione titolo e nome

--- Luis php/carrello.php
+++ Luis php/carrello.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'required.php';
 
-$SetParameters["titolo"] = "Carrello di "; // Inserire nome utente
+$SetParameters["titolo"] = "Carrello di ".$SetParameters["nome"]; // Inserire nome utente
 $SetParameters["file"] = "carrello_form.php";
 
 require("template/base.php");
