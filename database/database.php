6bfb6efd8ffb2477c27b26e2f60668d6371a1221
LuKe2_Ink <56109204+LuKe2Ink@users.noreply.github.com>
2022-01-25 18:52:10 +0100

    risolto, spero

--- database/database.php
+++ database/database.php
@@ -75,7 +75,7 @@ class DatabaseHelper{
     }
 
     public function randomProducts(){
-        $query = ("SELECT articolo.ID_Articolo, articolo.img FROM articolo ORDER BY RAND() LIMIT 3");
+        $query = ("SELECT articolo.ID_Articolo, articolo.img, articolo.quantità FROM articolo ORDER BY RAND() LIMIT 3");
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         $result = $stmt->get_result();
